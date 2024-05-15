468f9ddff6f4c369138824525e888b94e7f3cd48
Sebastian <astianmuchui@gmail.com>
2024-05-16 01:56:11 +0300

    Bugfix: Fixed ES Lint

--- utils/redis.js
+++ utils/redis.js
@@ -1,30 +1,30 @@
 const redis = require('redis');
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => console.log(`Redis client not connected to the server: ${err.message}`));
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const value = await this.client.get(key);
-        return value;
-    }
+  async get(key) {
+    const value = await this.client.get(key);
+    return value;
+  }
 
-    async set(key, value, duration) {
-        await this.client.set(key, value);
-        await this.client.expire(key, duration);
-    }
+  async set(key, value, duration) {
+    await this.client.set(key, value);
+    await this.client.expire(key, duration);
+  }
 
-    async del(key) {
-        await this.client.del(key);
-    }
+  async del(key) {
+    await this.client.del(key);
+  }
 }
 
-redisClient = new RedisClient();
+const redisClient = new RedisClient();
 
 module.exports = redisClient;
